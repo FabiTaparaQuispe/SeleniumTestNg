6a4858d0179b86548e8922042323b165787d75aa
FabiTaparaQuispe <ftapara@unsa.edu.pe>
2025-09-23 12:19:23 -0500

    Example with junit4 deprecated

---
+++ src/test/java/com/orange/tests/BaseTest.java
@@ -0,0 +1,29 @@
+package com.orange.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+    public WebDriver driver;
+    @Before
+    public void setup() {
+        System.out.println("9. Before Method");
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().setSize(new Dimension(1200, 940));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    }
+    @After
+    public void tearDown() {
+        if(driver != null) {
+            driver.quit();
+        }
+
+    }
+}
---
+++ src/test/java/com/orange/tests/LoginTest.java
@@ -0,0 +1,28 @@
+package com.orange.tests;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoginTest extends BaseTest {
+    @Test
+    public void doLogin(){
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).submit();
+
+        //assertion
+        WebElement lblDash = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[1]/span/h6")));
+        assertEquals(lblDash.getText(), "Dashboard");
+        //Assert.assertTrue(lblDash.getText().contains("Dashboard"));
+
+
+
+    }
+}
